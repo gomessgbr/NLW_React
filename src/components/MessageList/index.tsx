0d65384323f569f5642730cf1559fcba6e66a69d
Gabriel Gomes <gabrielgomes98@gmail.com>
2021-10-20 20:41:54 -0300

    add messages on screen from backend

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,24 +1,45 @@
 import styles from './styles.module.scss';
-
+import io from 'socket.io-client';
 
 
 import logoImg from '../../assets/logo.svg';
 import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
 
-
-export function MessageList() {
-
-    type Message= {
-        id: string,
-        text: string,
-        user: {
-            name: string,
-            avatar_url: string
-        }
+type Message= {
+    id: string,
+    text: string,
+    user: {
+        name: string,
+        avatar_url: string
     }
+}
+
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', newMessage => {
+    messagesQueue.push(newMessage)
+})
+
+export function MessageList() {    
 
     const [messages,setMessages] = useState<Message[]>([])
+
+    useEffect(()=>{
+        setInterval(() => {
+            if(messagesQueue.length> 0){
+                setMessages(prevState =>[
+                    messagesQueue[0],
+                    prevState[0],
+                    prevState[1],
+                ].filter(Boolean))
+
+                messagesQueue.shift();
+            }
+        },3000)
+    },[])
 
     useEffect(() => {
         api.get<Message[]>('messages/last3').then(response =>{
